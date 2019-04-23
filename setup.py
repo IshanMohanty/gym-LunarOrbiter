94b480ccf783808bf09ec8cabcf4d7dadc2816f0
Ishan Mohanty <ishanwiz1991@gmail.com>
2019-04-22 18:48:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='gym_lunar_orbiter',
       version='0.01',
-      url='https://github.com/IshanMohanty/gym-LunarOrbiter',
+      url='https://github.com/IshanMohanty/gym-lunar_orbiter',
       description='Lunar Orbiter',
       install_requires=['gym','box2d-py','numpy']
 )
